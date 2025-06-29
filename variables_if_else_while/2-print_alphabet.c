d69f2e9b7d5951e45862e17c0bd93a78f3e8b547
ArveenL <arveenmogun2019@gmail.com>
2025-06-29 00:40:34 +0000

    create a C program that prints the lowercase alphabet using only putchar twice

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,23 +1,3 @@
-/** Step 1 - understanding the requrements
-* Print lowercase alphabets(a-z)
-* Use onlu putchar function(not printf, puts, etc)
-*Use putchar exactly twice in the code
-*All code must be in main function
-*Follow Betty style
-*
-*Step 2: Planning the Solution
-*Use a for loop to iterate through letters  'a to z'
-* First putchar call: print each letter in the loop
-* Second putchar call: print the newline character '\n'
-*
-*Step 3 Betty style
-*Include proper header comment for the main fucntion
-*Use tab indentation(not spaces)
-*Declare variables at the beginning of the function
-*Use descriptive variables names
-*Follow proper spacing and formatting
-*/
-
 #include <stdio.h>
 
 /**
