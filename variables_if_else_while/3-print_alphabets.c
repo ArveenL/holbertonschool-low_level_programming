5caccbd425f001755d55890bdccffc6eb43f0fb5
ArveenL <arveenmogun2019@gmail.com>
2025-06-29 00:45:50 +0000

    create a C program that prints both lowercase and uppercase alphabets using only putchar three times.

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#inclide <stdio.h>
+
+/**
+* main  - prints the alphabet in lowercase, then in uppercase, folowed by a new line
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+for (c = 'A'; c <= 'Z'; c++)
+putchar(c);
+putchar('\n')
+return (0);
+}
