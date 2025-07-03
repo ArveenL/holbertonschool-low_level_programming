9ad505f9bb26266fe3de4048f32e700baa7f7dfb
ArveenL <arveenmogun2019@gmail.com>
2025-07-03 06:50:26 +0000

    print_most_numbers function - prints 0-9 except 2,4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,20 +1,20 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c);
-
-/**
- * print_most_numbers - prints numbers from 0 to 9 except 2 and 4
+ * print_most_numbers - prints numbers 0 to 9 except 2 and 4
  *
  * Return: void
  */
-void print_most_numbers(void);
-
-#endif /* MAIN_H */
+void print_most_numbers(void)
+{
+    int i;
+    
+    for (i = 0; i < 10; i++)
+    {
+        if (i != 2 && i != 4)
+        {
+            _putchar(i + '0');
+        }
+    }
+    _putchar('\n');
+}
