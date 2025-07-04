859465b75085121e56b7b1a1c2066efafa6bd24a
ArveenL <arveenmogun2019@gmail.com>
2025-07-04 08:59:03 +0000

    swaps value of two int

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*swap_int - swaps values between integers
+*@a: Points to first int
+*@b: Points to second int
+*
+*return: void
+*/
+void swap_int(int*a, int*b)
+{
+int temp;
+
+temp = *a;
+*a = *b;
+*b = temp;
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,10 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAIN_H /* header guard */
+#define MAIN_H /* prevents double inclusion */
 
+/* Function prototype: changes value of int to 98 */
 void reset_to_98(int *n);
 
-#endif
+/* Function prototype: swaps value of two ints */
+void swap_int(int *a, int *b);
+
+#endif /* closes header guard MAIN_H */
