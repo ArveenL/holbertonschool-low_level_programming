7d5bd6fa845086d1aa901b72c1d1551bfb8d4113
ArveenL <arveenmogun2019@gmail.com>
2025-07-04 08:32:37 +0000

    takes 'pointer to an integer' as parameter, and update it to another value(98)

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -10,3 +10,15 @@ void reset_to_98(int *n)
 {
 	*n = 98;
 }
+#include "main.h"
+
+/**
+ * reset_to_98 - Updates the value pointed to by a pointer to 98
+ * @n: Pointer to an int  that doesn't change value but address only
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
