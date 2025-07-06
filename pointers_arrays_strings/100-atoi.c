b547d3bcd1a510e74f650c0a2953f6e0c1f0778d
ArveenL <arveen.mogun2019@gmail.com>
2025-07-06 10:45:45 +0000

    corrections_2

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,60 +1,32 @@
 #include "main.h"
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
+ * _atoi - Converts a string to an integer.
+ * @s: The string to convert.
  *
- * Return: integer value of the string
+ * Return: The integer converted from the string.
  */
 int _atoi(char *s)
 {
 	int i = 0;
 	int sign = 1;
-	unsigned int result = 0;
-	int num_found = 0;
+	int num_started = 0;
+	int result = 0;
 
-	/* Skip non-numeric characters and handle signs */
 	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
-		{
 			sign *= -1;
-		}
-		else if (s[i] == '+')
-		{
-			/* Do nothing, sign remains the same */
-		}
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
-			num_found = 1;
-			/* Check for overflow before multiplication */
-			if (result > (2147483647 - (s[i] - '0')) / 10)
-			{
-				if (sign == 1)
-					return (2147483647);
-				else
-					return (-2147483648);
-			}
+			num_started = 1;
 			result = result * 10 + (s[i] - '0');
 		}
-		else if (num_found)
-		{
-			/* If we found a number and hit a non-digit, stop */
+		else if (num_started)
 			break;
-		}
 		i++;
 	}
 
-	/* Handle sign and potential overflow */
-	if (sign == -1)
-	{
-		if (result > 2147483648U)
-			return (-2147483648);
-		return (-(int)result);
-	}
-
-	if (result > 2147483647U)
-		return (2147483647);
-
-	return ((int)result);
+	return (sign * result);
 }
+
