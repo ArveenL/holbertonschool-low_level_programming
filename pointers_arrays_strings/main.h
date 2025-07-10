26e38ee504e7483cb8e6d6907b11d672a282ffd5
ArveenL <arveenmogun2019@gmail.com>
2025-07-10 08:05:24 +0000

    locates character in string

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,39 +1,37 @@
-#ifndef MAIN_H /* header guard */
-#define MAIN_H /* prevents double inclusion */
+#ifndef MAIN_H
+#define MAIN_H
 
-/* Function prototype: changes value of int to 98 */
-void reset_to_98(int *n);
+/* Basic I/O functions */
+int _putchar(char c);
+void _puts(char *str);
 
-/* Function prototype: swaps value of two ints */
-void swap_int(int *a, int *b);
-
-/* Function that counts lenght of string */
+/* String manipulation functions */
 int _strlen(char *s);
-
-int _putchar(char c);
-void _puts(char *str);
-int _putchar(char c);
-void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
-void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-/* Function prototypes(more) */
-int _putchar(char c);
+
+/* String concatenation and comparison functions */
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
+
+/* Array manipulation functions */
+void print_array(int *a, int n);
 void reverse_array(int *a, int n);
+
+/* String transformation functions */
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
-/* Function prototupe(more2) */
+
+/* Memory manipulation functions */
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 
-#endif /* closes header guard MAIN_H */
+#endif /* MAIN_H */
