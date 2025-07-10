6d3cc8dc6e33a7dd2e04ce77d70c70ed6ba25a43
ArveenL <arveenmogun2019@gmail.com>
2025-07-10 09:07:48 +0000

    ..

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -34,4 +34,13 @@ char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 
+/* Advanced string functions */
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+
+/* Printing functions */
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+
 #endif /* MAIN_H */
