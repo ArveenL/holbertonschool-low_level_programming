b3fdbe6c86162ca20c268f5dce46e537e1312146
ArveenL <arveenmogun2019@gmail.com>
2025-07-11 07:34:27 +0000

    prints string followed by /n

---
+++ recursion/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: pointer to the string to print
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s);
+
+#endif /* MAIN_H */
