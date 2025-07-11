5ad164fb1301dc8aa5a25caa7c98a33ea47bbd7f
ArveenL <arveenmogun2019@gmail.com>
2025-07-11 08:08:28 +0000

    print string in reverse

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - prints a string, in reverse
+ * _print_rev_recursion - prints a string in reverse
  * @s: pointer to the string to print
  *
  * Return: void
@@ -10,10 +10,9 @@ void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar('\n');
 		return;
 	}
 
-	_putchar(*s);
-	_print_rev_recursion(s + 1);
+	_print_rev_recursion(s + 1);  /* Go to end first */
+	_putchar(*s);                 /* Print on way back */
 }
