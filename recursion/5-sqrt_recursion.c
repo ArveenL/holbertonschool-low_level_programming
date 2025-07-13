063cb22f84873790e707712a978decf241e33a2c
ArveenL <arveenmogun2019@gmail.com>
2025-07-13 08:37:35 +0000

    all

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * sqrt_helper - helper function to find square root
+ * @n: number to find square root of
+ * @guess: current guess for square root
+ *
+ * Return: square root if found, -1 otherwise
+ */
+int sqrt_helper(int n, int guess)
+{
+    if (guess * guess == n)
+        return (guess);
+    if (guess * guess > n)
+        return (-1);
+    return (sqrt_helper(n, guess + 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to find square root of
+ *
+ * Return: natural square root, or -1 if no natural square root exists
+ */
+int _sqrt_recursion(int n)
+{
+    if (n < 0)
+        return (-1);
+    if (n == 0 || n == 1)
+        return (n);
+    return (sqrt_helper(n, 1));
+}
--- recursion/main.h
+++ recursion/main.h
@@ -1,21 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
 int _putchar(char c);
-
-/**
- * _puts_recursion - prints a string, followed by a new line
- * @s: pointer to the string to print
- *
- * Return: void
- */
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
-#endif /* MAIN_H */
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+
+#endif
