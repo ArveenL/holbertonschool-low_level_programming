221889959a64e5a11841cdf7c94903e531eeefd6
ArveenL <arveenmogun2019@gmail.com>
2025-07-13 09:06:49 +0000

    all

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * factorial - returns the factorial of a given number
- * @n: number to calculate factorial of
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: base number
+ * @y: exponent
  *
- * Return: factorial of n, or -1 if n is negative
+ * Return: x raised to power y, or -1 if y is negative
  */
-int factorial(int n)
+int _pow_recursion(int x, int y)
 {
-	if (n < 0)
+	if (y < 0)
 		return (-1);
-	if (n == 0 || n == 1)
+	if (y == 0)
 		return (1);
-	return (n * factorial(n - 1));
+	return (x * _pow_recursion(x, y - 1));
 }
--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,17 +1,30 @@
 #include "main.h"
 
 /**
- * _pow_recursion - returns the value of x raised to the power of y
- * @x: base number
- * @y: exponent
+ * sqrt_helper - helper function to find square root
+ * @n: number to find square root of
+ * @guess: current guess for square root
  *
- * Return: x raised to power y, or -1 if y is negative
+ * Return: square root if found, -1 otherwise
  */
-int _pow_recursion(int x, int y)
+int sqrt_helper(int n, int guess)
 {
-	if (y < 0)
+	if (guess * guess == n)
+		return (guess);
+	if (guess * guess > n)
 		return (-1);
-	if (y == 0)
-		return (1);
-	return (x * _pow_recursion(x, y - 1));
+	return (sqrt_helper(n, guess + 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to find square root of
+ *
+ * Return: natural square root, or -1 if no natural square root exists
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (sqrt_helper(n, 0));
 }
