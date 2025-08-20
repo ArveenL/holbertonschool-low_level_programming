91c4951013f2e035ff7d33120b55d546ddd465d2
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2025-08-20 13:26:59 +0400

    5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+void more_numbers(void){
+
+	int i; //row
+	int j; //column
+
+	for(i=0;i<=10;i++){
+	for(j=0;j<=14;j++)
+	}
+
+	while(j<10){
+	_putchar(j + '0');
+	{
+	
+	else{
+	_putchar('1' + (j%10);
+	{
+
+	putchar('\n');
+
+
+
+}
