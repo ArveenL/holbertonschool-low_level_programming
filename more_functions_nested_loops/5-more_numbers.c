2ec8ed06f5e222848a6eb17efed7d6b0ee78a2d8
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2025-08-20 13:47:34 +0400

    5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -7,17 +7,18 @@ void more_numbers(void){
 
 	for(i=0;i<=10;i++){
 	for(j=0;j<=14;j++)
-	
+
 
 	if(j<10){
 	_putchar(i + '0');
 	}
-	
+
 	else{
 	_putchar('1' + (j%10));
-	}
-	}
 	_putchar('\n');
+	}
+	}
+	
 
 
 
