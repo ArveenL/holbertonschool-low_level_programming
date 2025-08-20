34b841a0fb02f0a134439575d4d59178073f1ac9
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2025-08-20 08:37:22 +0400

    4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,21 +1,13 @@
 #include "main.h"
 
 void print_most_numbers(void){
-
 int i;
-	while(i=!2 && i=!4){
-	for (i=0;i<=9;i++){
-		
+
+	for(i=0;i<=9;i++){
+		while(i=!2 && i=!4){
 		_putchar(i+'0');
 		_putchar('\n');
 		}
-		}
-
-
-
-
-
-
-
+	}
 
 }
