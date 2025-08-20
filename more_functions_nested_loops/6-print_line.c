b21db70071917be5a217bc623d71c96530e6912a
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2025-08-20 20:15:33 +0400

    6

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -2,14 +2,13 @@
 
 void print_line(int n)
 {
-	char c='_';
-	
-	if(n<=0)
+	while(n>0)
 	{
-		_putchar('\n');
-	}
-	else
-	{
-		_putchar(c++);
+		_putchar('_');
+		n--;
+		if(n<=0)
+		{
+			_putchar('\n');
+		}
 	}
 }
