0134463fa8462573009e6ba13213e9694baaefec
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2025-08-21 00:29:49 +0400

    6

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,14 +1,22 @@
 #include "main.h"
 
+/**
+*print_line - draws line using _
+*@n : user input to determine how many _ to print
+*/
+
 void print_line(int n)
-{
-	while(n>0)
+{ 
+	int i;
+
+	if(n <= 0)
 	{
+		_putchar('\n');
+	}
+	else
+	{
+		for(i = 0; i < n; i++)
 		_putchar('_');
-		n--;
-		if(n<=0)
-		{
-			_putchar('\n');
-		}
 	}
 }
+
