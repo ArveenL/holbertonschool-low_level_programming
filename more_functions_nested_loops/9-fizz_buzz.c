4a8b86c4d61704e98919455b6bbd92439a912c38
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2025-08-21 18:05:26 +0400

    9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,18 +12,18 @@ int main(void)
 	int n;
 	for(n=1;n<=100;n++)
 	
-	if(n/3=0)
+	if(n/3==0)
 	{
 		printf("Fizz ");
 	}
-	else if(n/5)
+	else if(n/5==0)
 	{
 		printf("Buzz ");
 	}
-	else if(n/3 && n/5)
+	else if((n/3 && n/5)==0)
 	{
 		printf("FizzBuzz ");
 	}
-		return;
+		return 0;
 
 }
