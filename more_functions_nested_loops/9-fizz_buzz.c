63cff030c6edf48b6af87966530fbb6acdc03c9b
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2025-08-21 20:23:05 +0400

    9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-/**
-     * main - nothing special just main
-     * @n - for iterating 1-100
+ /**
+     * main - nothing special
+     * @ n: for iterating 1 - 100 
      *
      * Return: void
      */
@@ -10,23 +10,24 @@
 int main(void)
 {
 	int n;
-	for(n=1;n<=100;n++)
+	for(n=0;n<=100;n++)
 	{
-		if(n%3==0 && n%5==0)
-        	{
-                	printf("FizzBuzz ");
-        	}
-	
-			else if(n%3==0)
+		if(n%3 ==0 && n%5 ==0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if(n%3 ==0)
 		{
 			printf("Fizz ");
 		}
-
-			else if(n%5==0)
+		else if(n%5 ==0)
 		{
 			printf("Buzz ");
 		}
-
-	}	return 0;
-
+		else
+		{
+			printf("%d ",n);
+		}
+	}
+	return;
 }
