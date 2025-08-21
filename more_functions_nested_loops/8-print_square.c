80533d7d30acf62a4212a3d659e3a46057b71a44
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2025-08-21 17:00:33 +0400

    8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+     * void print_square(int size) - prints square
+     * @size - function parameter/user input  
+     * @i - width
+     * @j - lenght
+     * Return: returns 0 if size<=0
+     */
+
+void print_square(int size)
+{
+	int i,j;
+
+	if(size<=0);
+	{
+		_putchar('\n');
+		return;
+	}
+	
+	for(i=0;i<size;i++)
+	{
+		for(j=0;j<i;j++)
+		{
+			_putchar('#');
+			_putchar('\n');
+		}
+	}
+}
