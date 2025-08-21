3e548e8610de996378ee06c0ee3bbd602ad5f759
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2025-08-21 14:36:16 +0400

    7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+     * print_diagonal(int n) - prints diagonal
+     * @n - function parameter/user input
+     * @i - row iteration
+     * @j - column iteration
+     * Return: void
+     */
+
+void print_diagonal(int n)
+{
+	int i; 
+	int j;
+	
+	for (i=0; i<n; i++)
+	{
+		for (j=0; j<i; j++)
+		_putchar(' ');
+	}
+		_putchar('\');
+		_putchar('\n');
+}
