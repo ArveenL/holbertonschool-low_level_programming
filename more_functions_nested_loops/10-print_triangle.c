8b3153b62851a1cc3393536445b729477a034450
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2025-08-22 05:46:47 +0400

    10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+ /**
+     * void print_triangle - prints triangle
+     * @int size - parameter/user input
+     * @int i - row
+     * @int j - column
+     * Return: void
+     */
+
+void print_triangle(int size)
+{
+	int i;
+	int j;
+	
+	if size(<=0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	
+	for (i=o;i<size;i++)
+	{
+		for(j=0;j<i;j++)
+		{
+			_putchar('#');
+		}
+         }	
+	putchar('\n');
+	return;
+}
+
+
+
+
+
+
