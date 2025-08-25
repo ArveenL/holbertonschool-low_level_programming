becfe3b911855276d9c2ef1f3e7ac6d2ee171e67
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2025-08-25 13:11:43 +0400

    3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+ /**
+     * _puts - prints string followed by new line
+     * @str: parameter holds memory address of first character of the string
+     *
+     * Return: void
+     */
+
+void _puts(char *str)
+{
+	while(str=!'\n')
+	{
+		putchar(*str);
+		str++;
+	}
+	putchar('\n');
+
+	return;
+}
+
+
+
+
+
+
+}
