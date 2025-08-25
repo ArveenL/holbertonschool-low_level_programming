407bd2be186460b7d6d2124284673f79cef03d4e
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2025-08-25 11:00:33 +0400

    1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+     * swap_int - swaps values of two ints
+     * @a: first int
+     * @b: second int
+     *
+     * Return: void
+     */
+
+void swap_int(int *a, int *b)
+{
+	int *a = b;
+	
+	return;
+}
