846dc5d32f96b13714a82f191272cbe61484b370
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2025-08-25 12:37:56 +0400

    2

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
-     * _strlen(char *s) - length of a string
-     * @s: pointer to a character
+     * _strlen(char *s) - cal length of a string
+     * @s: pointer to a STRING
      *
      * Return: integer
      */
 
 int _strlen(char *s)
 {
-	int len = 0;
+	int len = 0; //counter to store length of a string
 
-	while(char[len]!=\0)
+	while(s[len]!=\0)
 	{
 		len++;
 	}
