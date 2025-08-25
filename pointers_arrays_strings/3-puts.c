f11a6c3fcf50fa6c293110a11b03f965c3e6aab4
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2025-08-25 14:29:08 +0400

    3

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,26 +1,17 @@
 #include "main.h"
 
- /**
-     * _puts - prints string followed by new line
-     * @str: parameter holds memory address of first character of the string
-     *
-     * Return: void
-     */
-
+/**
+ * _puts - prints string followed by new line
+ * @str: parameter holds memory address of first character of the string
+ *
+ * Return: void
+ */
 void _puts(char *str)
 {
-	while(*str != '\0')
-	{
-		_putchar(*str);
-		str++;
-	}
-	_putchar('\n');
-
-}
-
-
-
-
-
-
+    while (*str != '\0')
+    {
+        _putchar(*str);
+        str++;
+    }
+    _putchar('\n');
 }
