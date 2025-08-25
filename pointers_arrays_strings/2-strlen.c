d899465905247012572e80146a7c7bf1b50f0ec5
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2025-08-25 12:22:01 +0400

    2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+     * _strlen(char *s) - length of a string
+     * @s: pointer to a character
+     *
+     * Return: integer
+     */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while(char[len]!=\0)
+	{
+		len++;
+	}
+
+	return len;
+}
