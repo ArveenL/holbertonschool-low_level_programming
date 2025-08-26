23e6fd21ec84411fb3301b18a5e6c0f40ec34808
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2025-08-26 11:56:59 +0400

    5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+ /**
+     * rev_string - reverses a string
+     * @s: pointer to a character 
+     *
+     * Return: void
+     */
+
+void rev_string(char *s)
+{
+	int start = 0;
+	int end = s[start]-1;
+	int temp;
+	
+
+	temp = start;
+	start = end;
+	end = temp;
+
+}
