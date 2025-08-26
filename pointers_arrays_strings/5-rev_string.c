a659eade8e61634a671ee4599db3f7254ad2d75a
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2025-08-26 14:18:59 +0400

    5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -9,13 +9,20 @@
 
 void rev_string(char *s)
 {
-	int start = 0;
-	int end = s[start]-1;
-	int temp;
-	
+	int length = 0;
+	int i;
+		
 
-	temp = start;
-	start = end;
-	end = temp;
+	while(length != '\0')
+	{
+		length++;
+	}
+
+	for(i=0; i==length/2; i++)
+	{
+		char temp = s[i];
+		s[i] = s[length-1-i];
+		s[length-1-i] = temp;
+	}
 
 }
