4602c34bdb3a9bdbca3cab6670e59a18a8f08bcc
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2025-10-15 13:26:41 +0400

    4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - prints lowercase with exceptions
+* Return: 0
+*/
+
+int main(void)
+{
+	char low_ = 'a';
+
+	while (low_ <= 'z')
+	{
+		if (low_ != 'q' && low_ != 'e')
+		{
+			putchar (low_);
+			low_++;
+		}
+			putchar('\n');
+	}		return (0);
+}
