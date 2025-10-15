5c31aab1a55a97f8c75acbfbdffd892a99b21ba8
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2025-10-15 11:07:14 +0400

    2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - prints a-z
+* Return: 0
+*/
+
+int main(void)
+{
+	char lower = ('a');
+	while lower = (<='z')
+	{
+		lower++;
+		putchar(lower);
+	}
+		putchar('\n');
+		return (0);
+}
