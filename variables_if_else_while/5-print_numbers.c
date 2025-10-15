be873e7add406d9803d931b66d77445fed87d46c
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2025-10-15 14:45:19 +0400

    5

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 /**
-* main - prints 0-9
-* Return: 0
-*/
+ * main - prints numbers 0 to 9
+ * Return: 0
+ */
 
 int main(void)
 {
 	char number = '0';
 
 	while (number <= '9')
-		{
-			putchar(number);
-			number++;
-		}
-	putchar('\n');
-	return (0);
+    	{
+        	putchar(number);
+        	number++;
+        }
+    	putchar('\n');
+    	return (0);
 }
