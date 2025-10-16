e4adfcee837ffcfcbe39f296e7febc79d5b60e35
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2025-10-16 14:44:39 +0400

    1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+include <stdio.h>
+include "main.h"
+
+/**
+* main - lowercase _putchar
+* Return: 0
+*/
+
+int main(void)
+{
+	char c = a;
+
+	while(c <= z)
+	{
+		_putchar c;
+		c++;
+	}
+	return (0);
+}
