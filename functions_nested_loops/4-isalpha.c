1378419438caf7004676881fcc1a8562024376ab
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2025-10-19 15:01:20 +0400

    4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+ /**
+* _isalpha - checks if a character is lower/upper-case
+* @c: the character to check
+*
+* Return: 1 if lower/upper-case, 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+
+		return (1);
+
+	if (c >= 'A' && c <= 'Z')
+
+		return (1);
+
+	else
+
+		return (0);
+}
