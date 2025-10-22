155168ccf2c62b7516e3664f1b5c002754b308ae
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2025-10-22 15:01:36 +0400

    2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* mul - dogs
+* @a: more dogs
+* @b: even more dogs
+*
+* Return: a multi by b
+*/
+
+int mul(int a, int b)
+{
+	int x; /*multiplication*/
+
+	x = (a * b);
+	return (x);
+}
