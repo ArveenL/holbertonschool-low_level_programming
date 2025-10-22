6820b0f6b586b97df4a7386350d181b7f21c7087
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2025-10-22 13:58:55 +0400

    10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the sum of a and b
+ */
+
+int add(int a, int b)
+{
+	int result;
+
+	result = (a + b);
+	return (result);
+}
