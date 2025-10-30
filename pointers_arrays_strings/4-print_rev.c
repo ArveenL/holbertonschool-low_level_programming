1278857de820b28707e8f0d0d95ba0db28f0f0f5
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2025-10-30 10:37:11 +0400

    4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - print reverse
+ * @s: char s
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	for (len = len - 1; len >= 0; len--)
+	{
+		_putchar(s[len]);
+	}
+
+	putchar('\n');
+}
