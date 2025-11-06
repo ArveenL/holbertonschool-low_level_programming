0816fde29e4bedc54808de185a072c377f303ef8
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2025-11-06 20:08:40 +0400

    2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _strncpy -  copies as from position 0 instead of NULL
+* @dest: destination
+* @src: source
+* @n: n bytes
+*
+* Return: pointer to dest
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && i != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	if (src[i] < n)
+	{
+		dest[i] = '\0';
+	}
+
+	return (dest);
+}
