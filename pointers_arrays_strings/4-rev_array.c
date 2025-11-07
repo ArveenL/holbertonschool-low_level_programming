29650afdaa25deeec1c88bda5400af636809da93
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2025-11-07 10:58:44 +0400

    4

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - print revese of arrays
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int temp;
+	int j = (n - 1);
+
+	while (i < j)
+	{
+		temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+		i++;
+		j--;
+	}
+}
