3bfc4d0d8c3a67ad6e7e828fd914f8fac6415c4a
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2025-11-11 20:44:59 +0400

    0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - prints name of prog
+* @argc: arg count
+* @argv: arg string arrays
+*
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
