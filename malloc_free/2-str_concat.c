3c5a0d02482e13f5042ff10f41f8bc1320e0c45e
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2025-11-20 10:32:26 +0400

    2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -8,43 +8,34 @@
 * @s2: string 2
 *
 * Return: NULL if malloc fails, else return pointer to conca-string
-*/
+*
 
 char *str_concat(char *s1, char *s2)
 {
-	char *concat;
-	int i, j, k, l = 0;
+    char *concat;
+    int i = 0, j = 0, k, l = 0;
 
-	if (s1 == NULL)
-		s1 = "";
+    if (s1 == NULL)
+        s1 = "";
+    if (s2 == NULL)
+        s2 = "";
 
-	if (s2 == NULL)
-		s2 = "";
+    while (s1[i] != '\0')
+        i++;
+    while (s2[j] != '\0')
+        j++;
 
-	while (s1[i] != '\0')
-		i++;
+    concat = malloc((i + j + 1) * sizeof(char));
+    if (concat == NULL)
+        return NULL;
 
-	while (s2[j] != '\0')
-		j++;
+    for (k = 0; k < i; k++)
+        concat[l++] = s1[k];
 
-	concat = malloc((i + j + 1) * sizeof(char));
+    for (k = 0; k < j; k++)
+        concat[l++] = s2[k];
 
-	if (concat == NULL)
-		return (NULL);
+    concat[l] = '\0';
 
-	for (k = 0; k < i; k++)
-	{
-		concat[l] = s1[k];
-		l++;
-	}
-
-	for (k = 0; k < j; k++)
-	{
-		concat[l] = s2[k];
-		l++;
-	}
-
-	concat[l] = '\0';
-
-	return (concat);
+    return concat;
 }
