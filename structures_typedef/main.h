962d4cf73db3bc58d833a490ea0ec2e24d975e94
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2025-11-28 13:30:18 +0400

    1

--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -7,10 +7,22 @@ char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
+
+/**
+ * struct dog - A dog object
+ * @name: pointer to the dog's name
+ * @age: the dog's age in years
+ * @owner: pointer to the owner's name
+ */
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
+
+/* Prototype for init_dog */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
+#endif /* MYHEADER_H */
 
-
-
-
-#endif
