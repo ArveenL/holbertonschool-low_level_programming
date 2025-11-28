fe50aa43f119fff0cb67c36697323726fd03446f
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2025-11-28 13:38:26 +0400

    1

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,3 +1,6 @@
+#ifndef DOG_H
+#define DOG_H
+
 /**
  * struct dog - A dog object
  * @name: pointer to the dog's name
@@ -11,3 +14,13 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+
+
+
+
+
+
+#endif
