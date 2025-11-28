8757810b6ea8d7e40b36dd6411f74569dd067640
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2025-11-28 13:26:57 +0400

    1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -9,10 +9,8 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	// Store the name string pointer into the struct's name field
+
 	d->name = name;
-	// Store the age number into the struct's age field
 	d->age = age;
-	// Store the owner string pointer into the struct's owner field
 	d->owner = owner;
 }
