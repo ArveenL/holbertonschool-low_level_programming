237e7804905fe2b237c3c06230a9bb1605e04747
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2025-11-28 13:52:43 +0400

    1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,19 +1,16 @@
-#include <stdlib.h>
-#include "dog.h"
-
 /**
- * init_dogg - A dog object
- * @name: pointer to the dog's name
- * @age: the dog's age in years
- * @owner: pointer to the owner's name
+ * init_dog - Initialize a dog structure
+ * @d: Pointer to the dog structure to initialize
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
  *
- * Return: init_dog
+ * Return: void
  */
-
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
-	return;
+		return;
 
 	d->name = name;
 	d->age = age;
