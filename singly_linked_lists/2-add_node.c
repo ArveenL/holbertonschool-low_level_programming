d3e878185073857e0530c69e7e580f722e81d133
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2025-12-16 11:02:19 +0400

    2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -12,9 +12,11 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
+	unsigned int len = 0;
+
 	list_t *new_node;
 
-	if (head == NULL)
+	if (str == NULL)
 	return (NULL);
 
 	new_node = malloc(sizeof(list_t));
@@ -30,7 +32,10 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	new_node->len = strlen(str);
+	while (str[len] != '\0')
+	len++;
+
+	new_node->len = len;
 	new_node->next = *head;
 	*head = new_node;
 
