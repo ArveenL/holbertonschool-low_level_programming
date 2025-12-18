edd9fc0ce55af9ee3f916501814a9640c3ec28c7
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2025-12-18 13:31:59 +0400

    x

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,66 +1,51 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
-* string_nconcat - concatenates 2 strings
-* @s1: string 1
-* @s2: string 2
-* @n: number of bytes from s2 to concatenate
-*
-* Return: pointer to newly allocated string containing s1 + n bytes from s2. 
-	  Returns NULL on failure
-*/
-
+ * string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: maximum number of bytes to take from s2
+ *
+ * Return: pointer to newly allocated string, or NULL on failure
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len_s1 = 0; unsigned int len_s2 = 0;
-	unsigned int i = 0; unsigned int j = 0;
-	char *pointer;
+    unsigned int len1 = 0, len2 = 0, i, j;
+    char *result;
 
-	if (s1 == NULL)
-	{
-		s1 = "";
-	}
+    /* Treat NULL as empty string */
+    if (s1 == NULL)
+        s1 = "";
+    if (s2 == NULL)
+        s2 = "";
 
-	if (s2 == NULL)
-	{
-		s2 = "";
-	}
+    /* Calculate lengths of s1 and s2 */
+    while (s1[len1] != '\0')
+        len1++;
+    while (s2[len2] != '\0')
+        len2++;
 
-	while (s1[len_s1] != '\0')
-	{
-		len_s1++;
-	}
+    /* If n is greater than length of s2, use entire s2 */
+    if (n > len2)
+        n = len2;
 
-	while (s2[len_s2] != '\0')
-	{
-		len_s2++;
-	}
+    /* Allocate memory for concatenated string + null terminator */
+    result = malloc(sizeof(char) * (len1 + n + 1));
+    if (result == NULL)
+        return (NULL);
 
-	if (n >= len_s2)
-	{
-		n = len_s2;
-	}
+    /* Copy s1 to result */
+    for (i = 0; i < len1; i++)
+        result[i] = s1[i];
 
-	pointer = malloc(len_s1 + len_s2 + 1);
-	
-	if (pointer == NULL)
-	{
-		return (NULL);
-	}
-	
-	for (i = 0; i < len_s1; i++)
-	{
-		pointer[i] = s1[i];
-	}
+    /* Copy first n bytes of s2 to result */
+    for (j = 0; j < n; j++)
+        result[i + j] = s2[j];
 
-	for (j = 0; j < n; j++)
-	{
-		pointer[i + j] = s2[j];
-	}
+    /* Null-terminate the new string */
+    result[i + j] = '\0';
 
-	pointer[i + j] = '\0';
-	
-	return (pointer);
+    return (result);
 }
+
