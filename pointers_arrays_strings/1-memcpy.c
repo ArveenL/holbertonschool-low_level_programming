08ffa3eed57f796be961f0dbc0081d556ebeeb12
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2025-12-18 13:21:24 +0400

    x

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: pointer to the destination memory
+ * @src: pointer to the source memory
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+    unsigned int i;
+
+    i = 0;
+    while (i < n)
+    {
+        dest[i] = src[i];
+        i++;
+    }
+
+    return (dest);
+}
