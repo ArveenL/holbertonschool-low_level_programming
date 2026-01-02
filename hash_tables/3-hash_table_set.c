8ba79feab72a6cdbeae43770433df4df30f82ca1
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-01-03 03:14:56 +0400

    3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -17,30 +17,40 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *node;
 	hash_node_t *new_node;
 
-	if (ht == NULL || key == NULL || key == *'\0' || value == NULL)
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 	return (0);
 
-	index = key_index((unsigned char *)key, ht->size);
-
+	index = key_index((const unsigned char *)key, ht->size);
 	node = ht->array[index];
+
 	while (node != NULL)
 	{
-		if (node->key = key)
-		free(node->value);
+		if (strcmp(node->key, key) == 0) /* compares node key to input key */
+		{
+			free(node->value);
+		}
+
+		node = (node->next);
 	}
 
-	node->value = stardup(value);
-	return (1);
-
-	new_node = malloc(sizeo(hash_table_t));
+	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 	return (0);
 
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
-	new_node->next = ht->array[size];
-
+	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
-	
+
 	return (1);
 }
+
+
+
+
+
+
+
+
+
+
