7fd75ca7dadf0d2878cb9a8444e957accc0ae9f6
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-01-10 13:11:34 +0400

    c

---
+++ file_io/3-cp.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+
+/**
+ * error_exit - prints an error message to stderr and exits
+ * @code: exit code
+ * @msg: error message format
+ * @arg: argument for message
+ */
+void error_exit(int code, const char *msg, const char *arg)
+{
+    dprintf(STDERR_FILENO, msg, arg);
+    exit(code);
+}
+
+/**
+ * main - copies the content of a file to another file
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+    int fd_from, fd_to;
+    ssize_t n_read, n_written;
+    char buffer[1024];
+
+    if (argc != 3)
+        error_exit(97, "Usage: cp %s %s\n", "file_from file_to");
+
+    fd_from = open(argv[1], O_RDONLY);
+    if (fd_from == -1)
+        error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+
+    fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+    if (fd_to == -1)
+    {
+        if (close(fd_from) == -1)
+            dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+        error_exit(99, "Error: Can't write to %s\n", argv[2]);
+    }
+
+    while ((n_read = read(fd_from, buffer, sizeof(buffer))) > 0)
+    {
+        n_written = write(fd_to, buffer, n_read);
+        if (n_written != n_read)
+        {
+            if (close(fd_from) == -1)
+                dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+            if (close(fd_to) == -1)
+                dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+            error_exit(99, "Error: Can't write to %s\n", argv[2]);
+        }
+    }
+
+    if (n_read == -1)
+    {
+        if (close(fd_from) == -1)
+            dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+        if (close(fd_to) == -1)
+            dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+        error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+    }
+
+    if (close(fd_from) == -1)
+        error_exit(100, "Error: Can't close fd %d\n", argv[1]);
+    if (close(fd_to) == -1)
+        error_exit(100, "Error: Can't close fd %d\n", argv[2]);
+
+    return 0;
+}
+
